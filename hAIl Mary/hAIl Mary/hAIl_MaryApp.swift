6f38bdd6df8aa39378dfa737392ab7c34fc8bd91
Bhavya Rajput <bhavyarajput17@gmail.com>
2024-04-16 22:06:44 -0700

    set up

---
+++ hAIl Mary/hAIl Mary/hAIl_MaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  hAIl_MaryApp.swift
+//  hAIl Mary
+//
+//  Created by Bhavya on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct hAIl_MaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
